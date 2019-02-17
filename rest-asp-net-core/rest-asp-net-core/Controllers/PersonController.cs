b6f05a1fccf47e470df9af09d93c1178d952b7e7
Evandro Nuss <evandronuss@outlook.com>
2019-02-17 17:12:26 -0300

    limpeza

--- rest-asp-net-core/rest-asp-net-core/Controllers/PersonController.cs
+++ rest-asp-net-core/rest-asp-net-core/Controllers/PersonController.cs
@@ -8,7 +8,7 @@ namespace rest_asp_net_core.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class PersonController : ControllerBase
     {
         // GET api/values
         [HttpGet]
